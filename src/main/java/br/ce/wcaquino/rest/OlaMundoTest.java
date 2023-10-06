1d4ecaf12343c5687b7a22b2de1c00368fbd2b27
Marco Chaves <macschaves@gmail.com>
2023-10-06 09:48:47 -0300

    Correção da versão Maven e implementação Junit Test

--- src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
+++ src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -1,15 +1,18 @@
 package br.ce.wcaquino.rest;
 
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 
-public class OlaMundo {
+public class OlaMundoTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void testOlaMundo(){
         Response response = RestAssured.request (Method.GET, "https://stackoverflow.com/questions/67179242/noclassdeffounderror-getting-this-error-at-runtime");
         System.out.println(response.getBody().asString());
         System.out.println("Status code: " + response.statusCode());
+
     }
 }
