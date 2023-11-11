48498487155906e04df37d5ac70cf17a289f2a32
Marco Chaves <macschaves@gmail.com>
2023-11-11 12:43:32 -0300

    XPath

--- src/main/java/br/ce/wcaquino/rest/UsersXMLTest.java
+++ src/main/java/br/ce/wcaquino/rest/UsersXMLTest.java
@@ -1,11 +1,14 @@
 package br.ce.wcaquino.rest;
 
+
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasItems;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.Is.is;
 
 public class UsersXMLTest {
@@ -58,9 +61,43 @@ public class UsersXMLTest {
                 .body("users.user.findAll{it.name.toString().contains('n')}.name.", hasItems("Maria Joaquina", "Ana Julia"))
                 .body("users.user.salary.find{it != null}.toDouble()", is(1234.5678))
                 .body("users.user.age.collect{it.toInteger() * 2}", hasItems(40, 50, 60))
-                .body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"))
+                .body("users.user.name.findAll{it.toString().startsWith('Maria')}.collect{it.toString().toUpperCase()}", is("MARIA JOAQUINA"));
 
+    }
+   /* @Test
+    public void devoFazerPesquisasAvancadasComXmlEJava() {
+        ArrayList<NodeImpl> nomes = given()
+                .when()
+                .get("https://restapi.wcaquino.me/usersXML")
+                .then()
+                .statusCode(200)
+                .extract().path("users.user.name.findAll{it.toString().contains('n')}");
+                Assert.assertEquals("Maria Joaquina".toUpperCase(), nomes.get(0).toString().toUpperCase());
+                Assert.assertTrue("ANA JULIA".equalsIgnoreCase(nomes.get(1).toString()));
 
+        System.out.println();
+
+    }*/
+
+    @Test
+    public void devoFazerPesquisasAvancadasComXPath(){
+        given()
+                .when()
+                .get("https://restapi.wcaquino.me/usersXML")
+                .then()
+                .statusCode(200)
+                .body(hasXPath("count(/users/user)", is("3")))
+                .body(hasXPath("/users/user[@id = '2']"))
+                .body(hasXPath("//user[@id = '1']"))
+                .body(hasXPath("//name[text() = 'Luizinho']/../../name", is ("Ana Julia")))
+                .body(hasXPath("//name[text() = 'Ana Julia']/following-sibling::filhos", allOf (containsString("Zezinho"), containsString("Luizinho"))))
+                .body(hasXPath("/users/user/name", is("João da Silva")))
+                .body(hasXPath("/users/user[2]/name", is("Maria Joaquina")))
+                .body(hasXPath("//name", is("João da Silva")))
+                .body(hasXPath("/users/user[last()]/name", is("Ana Julia")))
+                .body(hasXPath("count(/users/user/name[contains(., 'n')])", is("2")))
+                .body(hasXPath("//user[age < 24]/name", is("Ana Julia")))
+                .body(hasXPath("//user[age > 20 and age <  30]/name", is("Maria Joaquina")))
         ;
 
     }
