39dc37d916b49e6022955abf9b797d26bc4be846
Marco Chaves <macschaves@gmail.com>
2023-11-16 17:20:44 -0300

    Removendo com DELET

--- src/main/java/br/ce/wcaquino/rest/VerbosTest.java
+++ src/main/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -108,6 +108,30 @@ public class VerbosTest {
                     .body("age", is(98))
                     .body("salary", is(1234.5678f));
     }
+    @Test
+    public void deveRemoverUsuario(){
+        given ()
+                .log().all()
+                .when()
+                    .delete("https://restapi.wcaquino.me/users/1")
+
+                .then()
+                    .log().all()
+                    .statusCode(204);
+    }
+
+    @Test
+    public void naoDeveRemoverUsuarioInexistente(){
+        given ()
+                .log().all()
+                .when()
+                .delete("https://restapi.wcaquino.me/users/1000")
+
+                .then()
+                .log().all()
+                .statusCode(400)
+        .body("error", is("Registro inexistente"));
+    }
 }
 
 
