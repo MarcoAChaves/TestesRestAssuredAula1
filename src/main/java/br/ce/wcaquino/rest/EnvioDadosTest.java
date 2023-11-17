08a08229cc44dc79f2f40db02494eac703420222
Marco Chaves <macschaves@gmail.com>
2023-11-17 15:13:37 -0300

    Query Parametrizável

--- src/main/java/br/ce/wcaquino/rest/EnvioDadosTest.java
+++ src/main/java/br/ce/wcaquino/rest/EnvioDadosTest.java
@@ -1,9 +1,12 @@
 package br.ce.wcaquino.rest;
 
 import io.restassured.http.ContentType;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 public class EnvioDadosTest {
 
@@ -17,5 +20,19 @@ public class EnvioDadosTest {
         .log().all()
         .statusCode(200)
     .contentType(ContentType.XML);
+    }
+
+    @Test
+    public void deveEnviarValorViaQueryParam(){
+        given()
+                .log().all()
+                .queryParam("format", "xml")
+                .when()
+                .get("https://restapi.wcaquino.me/v2/users")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.XML)
+        .contentType( containsString("utf-8"));
     }
 }
