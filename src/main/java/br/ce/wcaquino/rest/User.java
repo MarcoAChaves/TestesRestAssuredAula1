901b68dc54460c35ca1ba73f5eb34c89b22b945a
Marco Chaves <macschaves@gmail.com>
2023-11-17 08:33:31 -0300

    Deserializando Objeto

--- src/main/java/br/ce/wcaquino/rest/User.java
+++ src/main/java/br/ce/wcaquino/rest/User.java
@@ -5,6 +5,17 @@ public class User {
     private String name;
     private Integer age;
     private Double Salary;
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", Salary=" + Salary +
+                ", id=" + id +
+                '}';
+    }
 
     public String getName() {
         return name;
@@ -33,5 +44,23 @@ public class User {
     public User(String name, Integer age) {
         this.name = name;
         this.age = age;
+    }
+
+    /**
+     * get field
+     *
+     * @return id
+     */
+    public Long getId() {
+        return this.id;
+    }
+
+    /**
+     * set field
+     *
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
     }
 }
