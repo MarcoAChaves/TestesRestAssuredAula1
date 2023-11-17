ecf9e580b0b4468be1ab2bf4fbd908f8aa843ec4
Marco Chaves <macschaves@gmail.com>
2023-11-17 08:12:55 -0300

    Serializando MAP

---
+++ src/main/java/br/ce/wcaquino/rest/Serializacao.java
@@ -0,0 +1,34 @@
+package br.ce.wcaquino.rest;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+public class Serializacao {
+
+    @Test
+    public void deveSalvarUsuarioUsandoMap() {
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("name", "Usuario via map");
+        params.put("age", 25);
+
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body(params)
+                .when()
+                .post("https://restapi.wcaquino.me/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", is(notNullValue()))
+                .body("name", is("Usuario via map"))
+                .body("age", is(25));
+    }
+}
