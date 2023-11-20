8141e083daa9308f1ac1a966de04189a98a0b225
Marco Chaves <macschaves@gmail.com>
2023-11-20 17:43:40 -0300

    Acessando API com chave

--- src/main/java/br/ce/wcaquino/rest/AuthTest.java
+++ src/main/java/br/ce/wcaquino/rest/AuthTest.java
@@ -19,4 +19,23 @@ public class AuthTest {
                 .statusCode(200)
                 .body("name", is("Luke Skywalker"));
     }
+
+    @Test
+    public void deveObterClima(){
+        given()
+                .log().all()
+                .queryParam("q", "Fortaleza, BR")
+                .queryParam("appid", "369fb700fa828ff0dfdce8bcea766c74")
+                .queryParam("units", "metric")
+                .when()
+                .get("https://api.oppenweathermap.org/data/2.5/weather")
+                .then()
+                .log().all()
+                .statusCode(200)
+        .body("name", Matchers.is("Fortaleza"))
+        .body("coord.lon", is(-38.52f))
+        .body("main.temp", greaterThan(25f));
+    }
 }
+
+
