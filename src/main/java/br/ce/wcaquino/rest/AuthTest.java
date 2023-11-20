99fa41cfa8b593965f341e932f2740bfe7d1ab9a
Marco Chaves <macschaves@gmail.com>
2023-11-20 19:34:47 -0300

    Aplicação WEB

--- src/main/java/br/ce/wcaquino/rest/AuthTest.java
+++ src/main/java/br/ce/wcaquino/rest/AuthTest.java
@@ -1,6 +1,7 @@
 package br.ce.wcaquino.rest;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
@@ -134,6 +135,40 @@ public class AuthTest {
         .body("name", hasItem("Conta de teste"));
     }
 
+    @Test
+    public void deveAcessarAplicacaoWeb(){
+        //login
+        String cookie = given()
+                .log().all()
+                .formParam("email", "wagner@aquino")
+                .formParam("senha", "123456")
+                .contentType(ContentType.URLENC.withCharset("UTF-8"))
+                .when()
+                .post("http://seubarriga.wcaquino.me/logar")
+                .then()
+                .log().all()
+                .statusCode(200)
+        .extract().header("set-cookie");
+
+        cookie = cookie.split("=")[1].split(";")[0];
+        System.out.println("Cookie: " + cookie);
+
+        //Obter contas
+        String body = given()
+                .log().all()
+                .cookie("connect.sid", cookie)
+                .when()
+                .get("http://seubarriga.wcaquino.me/contas")
+                .then()
+                .log().all()
+                .statusCode(200)
+        .body("html.body.table.tbody.tr[0].td[0]", is("Conta 2533244560436800"))
+        .extract().body().asString();
+
+        System.out.println("--------------------------------");
+        XmlPath xmlPath = new XmlPath(XmlPath.CompatibilityMode.HTML, body);
+        System.out.println(xmlPath.getString("html.body.table.tbody.tr[0].td[0]"));
+    }
 
 }
 
