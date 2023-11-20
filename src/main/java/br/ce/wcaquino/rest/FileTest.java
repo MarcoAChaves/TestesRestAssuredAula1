8d98bb870f276cf6f375092fc2b69f7b6e075c71
Marco Chaves <macschaves@gmail.com>
2023-11-20 16:36:13 -0300

    Tempo maximo de teste e Dounload

--- src/main/java/br/ce/wcaquino/rest/FileTest.java
+++ src/main/java/br/ce/wcaquino/rest/FileTest.java
@@ -1,9 +1,11 @@
 package br.ce.wcaquino.rest;
 
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.registerParser;
@@ -35,5 +37,37 @@ public class FileTest {
                 .log().all()
                 .statusCode(200)
         .body("name", is("Teste.txt"));
+    }
+
+    @Test
+    public void naoDeveFazerUploadArquivoGrande(){
+
+        given()
+                .log().all()
+                .multiPart("arquivo", new File("C:\\Workspace/Teste2.txt"))
+                .when()
+                .post("http://restapi.wcaquino.me/upload")
+                .then()
+                .log().all()
+                .time(lessThan(3000L))
+                .statusCode(413);
+    }
+    @Test
+    public void deveBaixarArquivo() throws IOException {
+        byte[] image = given()
+                .log().all()
+                .when()
+                .get("http://restapi.wcaquino.me/download")
+                .then()
+                .log().all()
+                .statusCode(200)
+        .extract().asByteArray();
+
+        File imagem = new File("C\\Downloads/file.jpg");
+        OutputStream out = new FileOutputStream(imagem);
+        out.write(image);
+        out.close();
+
+        Assert.assertThat(imagem.length(), lessThan(100000L));
     }
 }
